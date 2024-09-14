bf96b322ed89fe78a7279ad41bef36c9b18ab470
rubel <rubel.xpress@gmail.com>
2024-09-14 20:08:17 +0600

    vector icons

--- App.tsx
+++ App.tsx
@@ -8,12 +8,17 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {DrawerActions, NavigationContainer, useNavigation} from '@react-navigation/native';
+import {
+  DrawerActions,
+  NavigationContainer,
+  useNavigation,
+} from '@react-navigation/native';
 import Home from './screens/Home';
 import Profile from './screens/Profile';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Pressable, Text} from 'react-native';
+import {Pressable} from 'react-native';
 import DrawerContent from './components/DrawerContent';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
@@ -29,7 +34,7 @@ function StackNavigations() {
               onPress={() => {
                 navigation.dispatch(DrawerActions.openDrawer());
               }}>
-              <Text>open</Text>
+              <Entypo name="menu" size={30} color="#000" />
             </Pressable>
           );
         },
--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import {  Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function DrawerContent() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>DrawerContent</Text>
-    </View>
+    </SafeAreaView>
   )
 }
